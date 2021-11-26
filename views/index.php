2e5e97eb1418f5c5fb7f8f64cf01e43ef5a3d996
EsTeBaN22001 <esteban1.redon2@gmail.com>
2021-11-26 02:17:46 -0300

    Se creó la sección de about de la página principal

---
+++ views/index.php
@@ -0,0 +1,46 @@
+<section class="hero">
+  <div class="hero-content container">
+    <h3 class="welcome">Bienvenido(a) a</h3>
+    <h1><a href="/" class="logo"><span>Carlos-</span>Guerra</a></h1>
+    <p>En esta página te ofrecemos los mejores productos <br> grabados de madera o metal. Ofrecemos productos de alta calidad.</p>
+  </div>
+</section>
+
+<section class="about section">
+  <div class="about-container container">
+    <div class="about-info">
+      <h2>Acerca de nosotros</h2>
+      <p>Nosotros lo que buscamos es hacer los productos de mayor calidad y al mejor precio. Contamos con una máquina de gran calidad para lograr hacer los mejores grabados con mayor calidad y en distintos materiales.</p>
+      <h3>Nuestros objetivos</h3>
+      <ul class="about-list">
+        <li class="list-item">Realizar trabajos de alta calidad</li>
+        <li class="list-item">Realizar los trabajos en el menor tiempo posible</li>
+        <li class="list-item">Hacer realidad las ideas de los clientes</li>
+        <li class="list-item">Lograr precios accesibles para todas las personas</li>
+        <li class="list-item">Holaa no se que poner. XD</li>
+      </ul>
+
+    </div>
+    <div class="about-image">
+      <img src="build/img/about.webp" alt="Imagen acerca de nosotros">
+    </div>
+  </div>
+</section>
+
+<section class="typeJobs section">
+  <div class="typeJobs-container container">
+    <h2>Tipos de trabajo</h2>
+    <p>Estos son los distintos tipos de trabajos que se pueden llegar a realizar. Pero ante una duda consulte a la empresa.</p>
+    <div class="jobs-container">
+      <div class="job">
+
+      </div>
+      <div class="job">
+
+      </div>
+      <div class="job">
+
+      </div>
+    </div>
+  </div>
+</section>
